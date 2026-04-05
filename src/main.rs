fdf6307f1f74e9e5aa957d39a041afb319a127fa
Daniele Basile <asterix@develer.com>
2026-04-06 00:32:07 +0200

    add sipo support and command

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ use cyw43::{JoinOptions, aligned_bytes};
 use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
 
 use embassy_executor::Spawner;
+use embassy_net::StackResources;
 use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, StackResources};
 use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0, USB};
+use embassy_rp::peripherals::{DMA_CH0, PIO0, SPI0, USB};
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::spi::{Blocking, Phase, Polarity, Spi};
 use embassy_rp::{bind_interrupts, dma, usb};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 use embedded_io_async::Write;
@@ -48,6 +49,24 @@ const WIFI_PASSWORD: &str = env!("PASSWORD");
 
 static PROTO_PARSE: Channel<CriticalSectionRawMutex, String<128>, 2> = Channel::new();
 static PROTO_RET: Channel<CriticalSectionRawMutex, String<64>, 2> = Channel::new();
+static SIPO_OUT: Channel<CriticalSectionRawMutex, u8, 2> = Channel::new();
+
+#[embassy_executor::task]
+async fn sipo_task(mut sipo: Spi<'static, SPI0, Blocking>, mut latch: Output<'static>) {
+    loop {
+        let data = SIPO_OUT.receive().await;
+        if let Ok(_) = sipo.blocking_write(&mut [data]) {
+            embassy_time::Timer::after_micros(1).await;
+            latch.set_level(Level::High);
+            embassy_time::Timer::after_micros(1).await;
+            latch.set_level(Level::Low);
+        } else {
+            error!("unable to write sipo");
+        }
+
+        embassy_time::Timer::after_millis(500).await;
+    }
+}
 
 #[embassy_executor::task]
 async fn logger_task(usb: embassy_rp::Peri<'static, embassy_rp::peripherals::USB>) {
@@ -85,6 +104,9 @@ async fn connection_task(stack: &'static NetStack, mut control: NetControl) -> !
 
             // And now we can use it!
             info!("Stack is up!");
+            if let Some(config) = stack.config_v4() {
+                info!("IP: {}", config.address);
+            }
             control.gpio_set(0, false).await;
         }
         embassy_time::Timer::after_secs(5).await;
@@ -147,7 +169,7 @@ async fn main(spawner: Spawner) {
     let clm = aligned_bytes!("../firmware/43439A0_clm.bin");
     let nvram = aligned_bytes!("../firmware/nvram_rp2040.bin");
 
-    //let led = Output::new(p.PIN_16, Level::Low);
+    let led = Output::new(p.PIN_22, Level::Low);
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
@@ -162,6 +184,22 @@ async fn main(spawner: Spawner) {
         dma::Channel::new(p.DMA_CH0, Irqs),
     );
 
+    // Sipo and Piso constants
+    //sipoEnable machine.Pin = machine.GPIO15
+    //sipoLoad               = machine.GPIO17
+    //sipoClk                = machine.GPIO18
+    //sipoMosi               = machine.GPIO19
+    //pisoMiso               = machine.GPIO16
+    //pisoLoad               = machine.GPIO14
+    let sipo_load = Output::new(p.PIN_17, Level::Low);
+    let mut sipo_en = Output::new(p.PIN_15, Level::High);
+    let mut sipo_config = embassy_rp::spi::Config::default();
+    sipo_config.frequency = 1_000_000;
+    sipo_config.phase = Phase::CaptureOnFirstTransition;
+    sipo_config.polarity = Polarity::IdleLow;
+    let sipo = Spi::new_blocking_txonly(p.SPI0, p.PIN_18, p.PIN_19, sipo_config);
+    sipo_en.set_level(Level::Low);
+
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
@@ -173,7 +211,7 @@ async fn main(spawner: Spawner) {
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
 
-    let config = Config::dhcpv4(Default::default());
+    let config = embassy_net::Config::dhcpv4(Default::default());
     // Generate random seed
     let seed = rng.next_u64();
 
@@ -192,17 +230,23 @@ async fn main(spawner: Spawner) {
     spawner.spawn(unwrap!(net_task(runner)));
     spawner.spawn(unwrap!(connection_task(stack, control)));
     spawner.spawn(unwrap!(shell_task(stack)));
+    spawner.spawn(unwrap!(sipo_task(sipo, sipo_load)));
 
     let in_chan = PROTO_PARSE.dyn_receiver();
     let out_chan = PROTO_RET.dyn_sender();
+    let sipo_out = SIPO_OUT.dyn_sender();
 
     loop {
         let return_msg = match Command::try_from(in_chan.receive().await) {
             Ok(cmd) => match cmd {
                 Command::Version => reply(env!("CARGO_PKG_VERSION"), true),
+                Command::Out { mask } => {
+                    sipo_out.send(mask).await;
+                    reply("", true)
+                }
                 _ => reply("Invalid Command", true),
             },
-            Err(e) => reply(format!("Uknow Connand: {}", e), true),
+            Err(_) => reply("Uknow Connand: {}", true),
         };
         out_chan.send(return_msg).await;
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,20 @@
 use core::str::FromStr;
 use heapless::String;
 
+fn hex_to_u8(input: &str) -> Option<u8> {
+    let pulita = input.trim();
+    let solo_hex = pulita
+        .strip_prefix("0x")
+        .or_else(|| pulita.strip_prefix("0X"))
+        .unwrap_or(pulita);
+    u8::from_str_radix(solo_hex, 16).ok()
+}
+
 #[derive(Debug)]
 pub enum Command {
     Ping { payload: String<16> },
     Led { on: bool },
+    Out { mask: u8 },
     Version,
 }
 
@@ -27,6 +37,14 @@ impl TryFrom<String<128>> for Command {
                 let arg = tokens.next().ok_or("Missing LED state")?;
                 let on = arg == "on" || arg == "1";
                 Ok(Command::Led { on })
+            }
+            "out" => {
+                let arg = tokens.next().ok_or("Missing out bit mask")?;
+                if let Some(mask) = hex_to_u8(arg) {
+                    Ok(Command::Out { mask })
+                } else {
+                    Err("Invalid input")
+                }
             }
             "version" => Ok(Command::Version),
             _ => Err("Unknown command"),
